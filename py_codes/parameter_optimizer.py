a08008644d67c4d0aea7edb19ae46ec36c30847d
nick-harries <nicholas.harries@ucalgary.ca>
2024-07-02 11:11:07 -0400

    Updated documentation and added if condition so frequencies array is only hard-coded if it cannot be extracted from the metadata

--- py_codes/parameter_optimizer.py
+++ py_codes/parameter_optimizer.py
@@ -4,6 +4,26 @@ import numpy as np
 from scipy.optimize import minimize
 import re
 
+# Read the file that you have set up with your directory paths
+txt_file_path = 'directories.txt'
+with open(txt_file_path, 'r') as file:
+    txt_content = file.read() #Search within the directories.txt file
+config_dir_match = re.search(r"Configuration files directory:\s*'(.+?)'", txt_content) #Find configuration file path
+fits_dir_match = re.search(r"Fits files directory:\s*'(.+?)'", txt_content) #Find Fits file directory path
+
+#Troubleshoot to make sure that the directory paths exist before executing rest of code
+if config_dir_match and fits_dir_match:
+    config_directory = config_dir_match.group(1)
+    fits_directory = fits_dir_match.group(1)
+else: #If the directory paths do not exist, print error message and stop code
+    raise ValueError("Directory paths not found in the tex file.")
+    exit()
+# Add the configuration directory to the system path
+sys.path.append(config_directory)
+
+# Import the necessary functions from the associated codes.
+from data_processing import extract_info, extract_constants_and_parameters, stokes_arrays_in_MJy_sr, decrease_resolution, conversion_factors_Kcmb_to_MJy_sr, blackbody_function, stokes_reconstruction, Chi2, define_bounds, extract_number
+from plot_functions import plot_recreated_values_and_fit, plot_optimized_parameters, plot_optimized_parameters_histograms, plot_minimized_Chi2
 
 
 """ 
@@ -13,30 +33,10 @@ The preamble creates the frequencies array, bounds list, calls the necessary fun
 The bounds are defined in the data_processing code and can be changed there. The only variable that might need to be changed on this code is the 'nside', which is the resolution that you degrade the arrays to.
 """
 
-
-
-# Read the file that you have set up with your directory paths
-tex_file_path = 'directories.tex'
-with open(tex_file_path, 'r') as file:
-    tex_content = file.read()
-config_dir_match = re.search(r"Configuration files directory:\s*'(.+?)'", tex_content)
-fits_dir_match = re.search(r"Fits files directory:\s*'(.+?)'", tex_content)
-# Define the directories
-if config_dir_match and fits_dir_match:
-    config_directory = config_dir_match.group(1)
-    fits_directory = fits_dir_match.group(1)
-else:
-    raise ValueError("Directory paths not found in the tex file.")
-# Add the configuration directory to the system path
-sys.path.append(config_directory)
-
-# Import the necessary functions from the associated codes.
-from data_processing import extract_info, extract_constants_and_parameters, stokes_arrays_in_MJy_sr, decrease_resolution, conversion_factors_Kcmb_to_MJy_sr, blackbody_function, stokes_reconstruction, Chi2, define_bounds, extract_number
-from plot_functions import plot_recreated_values_and_fit, plot_optimized_parameters, plot_optimized_parameters_histograms, plot_minimized_Chi2
-
 # List all FITS files in the directory and create 'files' array to be used to extract stokes arrays
 fits_files = [file for file in os.listdir(fits_directory) if file.endswith('.fits')]
-fits_files = sorted(fits_files, key = extract_number)
+fits_files = sorted(fits_files, key = extract_number) #Frequency maps from PLA have frequencies in the name, sort through files and open them in order of increasing frequency
+#The line above is necessary because CMB-removed maps do not have frequency values within the metadata
 files = [os.path.join(fits_directory, file) for file in fits_files]
 
 
@@ -50,16 +50,16 @@ bounds = define_bounds()
 #Create array of frequency values associated to the fits files
 frequencies = np.zeros(len(files))
 for f in range(len(files)):
-    frequencies[f] = np.float64(extract_info(files[f])[6])
-frequencies = np.array([1.43e11, 2.17e11, 3.43e11, 5.45e11, 8.57e11])
-print(frequencies)
+    frequencies[f] = np.float64(extract_info(files[f])[6]) #Sort through the fits files and append frequency values extracted from metadata
+if any(np.isnan(frequencies)): #CMB-removed maps have no frequency information in their metadata, this condition assumes frequencies associated with files in that case, should be modified to fit your condition
+    print('Frequencies not in metadata, using hard-coded values')
+    frequencies = np.array([1.43e11, 2.17e11, 3.43e11, 5.45e11, 8.57e11])
+print('Frequencies: ',frequencies)
 
 #Define nside resolution to change stokes arrays to, then extract information from fits files, change units to MJy/sr, change resolution, and store array
 
-
-
 nside = 128
-print('nside: ', nside)
+print('Converting to nside: ', nside)
 nest_type = extract_info(files[0])[-1]
 
 print('creating arrays ...')
@@ -88,7 +88,7 @@ def execute_Chi2_optimization(constants, stokes_arrays_correct_units, bounds, pa
 
     #Create list of which indices contain numerical data and should be used to calculate Chi2
 
-    print('create coordinates list...')
+    print('Finding array coordinates with data...')
     arrays_to_optimize = []
     for file_number in range(np.shape(stokes_arrays_correct_units)[0]):
         for parameter in range(3):
@@ -97,15 +97,15 @@ def execute_Chi2_optimization(constants, stokes_arrays_correct_units, bounds, pa
     print(arrays_to_optimize)   
    
 
-    length_of_sample = int(len(stokes_arrays_correct_units[0, 0, :]))
-    starting_index = int(0)
-    ending_index = int(starting_index + length_of_sample)
+    length_of_sample = int(len(stokes_arrays_correct_units[0, 0, :])) #Choose how many indices to optimize in this sample
+    starting_index = int(0) #Which index of the array to start at
+    ending_index = int(starting_index + length_of_sample) #Which index to end at (automatically calculated from length_of_sample and starting_index variables
 
-    stokes_arrays_reconstructed = np.zeros((len(frequencies), 3, length_of_sample))
-    optimized_parameters_array = np.zeros((length_of_sample, 7))
+    stokes_arrays_reconstructed = np.zeros((len(frequencies), 3, length_of_sample)) #initialize empty array to store stokes array models
+    optimized_parameters_array = np.zeros((length_of_sample, 7)) #Initialize empty array to store optimized parameters for each pixel of each optimized array
 
-    optimize_methods = ['BFGS', 'TNC', 'Powell']
-    tally_count = [0, 0, 0, 0]
+    optimize_methods = ['BFGS', 'TNC', 'Powell'] #scipy.optimize.minimize methods, all are used to find best possible parameter combination
+    tally_count = np.array([0, 0, 0, 0]) #Used to see which optimization methods are most efficient
 
     print('begin optimization, starting index: %d, ending index: %d ...'%(starting_index, ending_index))
     j = 0
@@ -114,7 +114,7 @@ def execute_Chi2_optimization(constants, stokes_arrays_correct_units, bounds, pa
         optimized_params = minimize(Chi2, parameters, args=(frequencies, constants, stokes_arrays_correct_units, arrays_to_optimize, i), method='Nelder-Mead')#, bounds=bounds)
         minimized_Chi_2 = optimized_params.fun
 
-        for idx, method in enumerate(optimize_methods):
+        for idx, method in enumerate(optimize_methods): #Cycle through optimizer methods, if new method returns smaller Chi2 value, store its parameters, ignore output in case of ValueErrors
             try:
                 # Alternate optimization with different methods, see which returns smallest Chi2 value
                 alternate_optimization = minimize(Chi2, parameters, args=(frequencies, constants, stokes_arrays_correct_units, arrays_to_optimize, i), method=method) #, bounds=bounds)
@@ -140,7 +140,7 @@ def execute_Chi2_optimization(constants, stokes_arrays_correct_units, bounds, pa
         print(j, '%.3e'%minimized_Chi_2)
         j+=1 #Update index number for storing in arrays
 
-    tally_count[0] = length_of_sample - tally_count[1] - tally_count[2] - tally_count[3] #Number of times the initial method is used is the length of the array minus the number of times the other methods were used.
+    tally_count[0] = length_of_sample - np.sum(tally_count) #Number of times the initial method is used is the length of the array minus the number of times the other methods were used.
     print('tally count: ', tally_count)
 
     return stokes_arrays_reconstructed, optimized_parameters_array, arrays_to_optimize, starting_index, ending_index
@@ -148,8 +148,8 @@ def execute_Chi2_optimization(constants, stokes_arrays_correct_units, bounds, pa
 stokes_arrays_reconstructed, optimized_parameters_array, arrays_to_optimize, starting_index, ending_index = execute_Chi2_optimization(constants, stokes_arrays_correct_units, bounds, parameters, frequencies)
 print(np.shape(stokes_arrays_reconstructed))
 print(np.shape(optimized_parameters_array))
-np.save('Stokes_Arrays_CMB_Removed_nside_%d'%nside, stokes_arrays_reconstructed)
-np.save('Parameters_Arrays_CMB_Removed_nside_%d'%nside, optimized_parameters_array)
+#np.save('Stokes_Arrays_CMB_Removed_nside_%d'%nside, stokes_arrays_reconstructed)
+#np.save('Parameters_Arrays_CMB_Removed_nside_%d'%nside, optimized_parameters_array)
 
 print('Plotting ...')
 plot_recreated_values_and_fit(stokes_arrays_reconstructed, stokes_arrays_correct_units, arrays_to_optimize, starting_index, ending_index, frequencies)
